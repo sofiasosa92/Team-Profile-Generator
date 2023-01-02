db9409f9b6087f1b684ef0ea88098a99b6af40b9
Sofia92 <sofiasosaperez@outlook.com>
2023-01-01 18:10:01 -0600

    added files to test

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-
 // packages needed for this application
 const inquirer = require('inquirer');
+const path = require('path');
 const fs = require('fs');
-const util = require('util');
 
-const writeFileAsync = util.promisify(fs.writeFile);
 
-//terminal messages
 
 console.log("Welcome, please enter your information to the team roster:")
 
@@ -18,3 +15,37 @@ const questions = () =>
             name: "name",
             message: "name:",
         }]);
+        
+//classes (manager, engineer, intern)
+
+//output directory
+
+//output file path and name
+
+//import HTML 
+
+//array of team members
+const teamMembers = [];
+
+//Team members:
+
+
+//Types of members:
+//Manager input
+
+//Engineer input
+
+//Intern input
+
+
+
+
+//HTML file
+
+
+//Deploy application
+
+
+
+//terminal messages
+
